7a6d766a26c7a6e6621f58c6985069e1d7773326
Zhivko76 <zkzhelyazkov@gmail.com>
2020-12-11 18:40:25 +0200

    UserProfile refractored to use CBV

--- petstagram/accounts/urls.py
+++ petstagram/accounts/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path, include
 
-from petstagram.accounts.views import user_profile, SignUpView, SignOutView, SignInView  # , signup_user, signout_user
+from petstagram.accounts.views import SignUpView, SignOutView, SignInView, \
+    UserProfileView  # ,user_profile, signup_user, signout_user
 
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
-    path('profile/', user_profile, name='current user profile'),
-    path('profile/<int:pk>/', user_profile, name='user profile'),
+    # path('profile/', user_profile, name='current user profile'),
+    path('profile/', UserProfileView.as_view(), name='current user profile'),
+    # path('profile/<int:pk>/', user_profile, name='user profile'),
+    path('profile/<int:pk>/', UserProfileView.as_view(), name='user profile'),
     path('signin/', SignInView.as_view(), name='signin user'),
     # path('signup/', signup_user, name='signup user'),
     path('signup/', SignUpView.as_view(), name='signup user'),
