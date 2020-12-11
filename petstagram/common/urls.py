719e896bf34b4662eff8777c951e343b86ff2942
Zhivko76 <zkzhelyazkov@gmail.com>
2020-12-11 21:47:18 +0200

    LandingPage is now CBV

--- petstagram/common/urls.py
+++ petstagram/common/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from petstagram.common.views import landing_page
+from petstagram.common.views import LandingPage  # landing_page,
 
 urlpatterns = [
-    path('', landing_page, name='index'),
+    # path('', landing_page, name='index'),
+    path('', LandingPage.as_view(), name='index'),
 ]
--- petstagram/common/views.py
+++ petstagram/common/views.py
@@ -1,5 +1,10 @@
+from django.views import generic as views
 from django.shortcuts import render
 
 
-def landing_page(request):
-    return render(request, 'landing_page.html')
+# def landing_page(request):
+#     return render(request, 'landing_page.html')
+
+
+class LandingPage(views.TemplateView):
+    template_name = 'landing_page.html'
